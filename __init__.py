2c375a73f5bb540f872eea94222b944e50491edc
Alan Degenhart <alandegenhart@gmail.com>
2020-04-22 20:22:49 -0700

    Updating package init files to reflect new package structure

--- __init__.py
+++ __init__.py
@@ -1,2 +1,13 @@
-"""Neuropy top-level init."""
+"""NeuroPy
+
+A neural analysis package implemented in Python.
+
+"""
+
+# Import sub-packages
 import neuropy.util
+import neuropy.analysis
+import neuropy.el
+
+# Import top-level modules
+import neuropy.array
--- util/__init__.py
+++ util/__init__.py
@@ -1,2 +1,7 @@
-"""Neuropy utilities init."""
+"""Neuropy utilities package.
+
+This package contains various utilities modules.
+"""
+
+# Import modules
 import neuropy.util.convertmat
