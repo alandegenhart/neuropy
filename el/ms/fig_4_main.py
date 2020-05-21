7e6f208642759b75761ca29fb7644c4bf3609e02
Alan Degenhart <alandegenhart@gmail.com>
2020-05-20 18:02:32 -0700

    Updating main analysis to be consistent with changes to main fig 4 analysis module. Also adding temporary plotting of results.

--- el/ms/fig_4_main.py
+++ el/ms/fig_4_main.py
@@ -20,16 +20,20 @@ dataset = '20180927'
 
 # Define parameter dictionary
 params = {
-    'projection_mode': ['orth', 'random'],
+    'projection_mode': ['random'],
     'n_proj': [10],
-    'n_permute': [25],
-    'grid_delta': [1, 2],
-    'grid_n_min': [2],
-    'n_proj_plots': [5]
+    'n_permute': [10],
+    'grid_delta': [2],
+    'grid_n_min': [1],
+    'n_proj_plots': [20]
 }
 
 # Define possible parameter sets and run analysis
 param_sets = f4.define_param_sets(params)
 for p_set in param_sets:
-    f4.flow_analysis(subject, dataset, p_set, base_dir)
+    results, flow_ex, comp_cond = f4.flow_analysis(
+        subject, dataset, p_set, base_dir)
+
+# Create plots for flow examples
+f4.plot_flow_ex(subject, dataset, flow_ex, comp_cond, p_set)
 
