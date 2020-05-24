a02b3cee0159a69392fcd3b6a7869899da82f3bb
Alan Degenhart <alandegenhart@gmail.com>
2020-05-24 10:53:25 -0700

    Adding multiprocessing, command line argument functionality.

--- el/ms/fig_4_main.py
+++ el/ms/fig_4_main.py
@@ -1,9 +1,33 @@
+"""Figure 4 main analysis
+
+"""
+
+
 # Import libraries
 import os
+import sys
 import fig_4_module as f4
+import multiprocessing as mp
+
+
+def main_fun(args):
+    """Main analysis function for a single parameter set."""
+    results, flow_ex, comp_cond = f4.flow_analysis(
+        args['subject'], args['dataset'], args['params'], args['dir'])
+    f4.plot_flow_ex(
+        args['subject'], args['dataset'], flow_ex, comp_cond, args['params'])
+    f4.plot_flow_results(
+        args['subject'], args['dataset'], results, args['params'])
+
+
+# Check input arguments. Currently this script accepts one input argument, which
+# specifies whether to use the local or remote path.
+if len(sys.argv) == 1:
+    mode = 'local'
+else:
+    mode = sys.argv[1]
 
 # Define data path
-mode = 'local'
 if mode == 'local':
     base_dir = os.path.join(
         os.sep, 'Volumes', 'Samsung_T5', 'Batista', 'Animals'
@@ -17,23 +41,37 @@ elif mode == 'remote':
 # Define subject and dataset
 subject = 'Earl'
 dataset = '20180927'
+use_multiproc = True
+pool_size = 8
 
 # Define parameter dictionary
 params = {
-    'projection_mode': ['random'],
+    'projection_mode': ['random', 'orth'],
     'n_proj': [10],
     'n_permute': [10],
-    'grid_delta': [2],
-    'grid_n_min': [1],
+    'grid_delta': [1.5],
+    'grid_n_min': [2],
     'n_proj_plots': [20]
 }
 
-# Define possible parameter sets and run analysis
+# Define possible parameter sets. Create an array of dicts to pass to the main
+# function in order to use multiprocessing if desired.
 param_sets = f4.define_param_sets(params)
-for p_set in param_sets:
-    results, flow_ex, comp_cond = f4.flow_analysis(
-        subject, dataset, p_set, base_dir)
+all_params = [
+    {
+        'subject': subject,
+        'dataset': dataset,
+        'dir': base_dir,
+        'params': p_set
+    }
+    for p_set in param_sets
+]
 
-# Create plots for flow examples
-f4.plot_flow_ex(subject, dataset, flow_ex, comp_cond, p_set)
+if use_multiproc:
+    with mp.Pool(processes=pool_size) as pool:
+        pool.map(main_fun, all_params)
+else:
+    for params in all_params:
+        main_fun(params)
+
 
