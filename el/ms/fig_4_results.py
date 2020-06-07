03093b21f7c3d853c932e507b690ad924cd93ce1
Alan Degenhart <alandegenhart@gmail.com>
2020-06-07 09:24:29 -0700

    Adding plotting of results for all parameter set directories.

--- el/ms/fig_4_results.py
+++ el/ms/fig_4_results.py
@@ -2,6 +2,7 @@
 
 # Import
 import os
+import pathlib
 import sys
 import fig_4_module as f4
 
@@ -12,6 +13,7 @@ import fig_4_module as f4
 
 # Define path to data
 results_dir = 'Earl_20180927_projMode_random_nProj_500_nPerm_100_gridDelta_1_gridNMin_1'
+results_dir = 'Earl_20180927_projMode_random_nProj_500_nPerm_100_gridDelta_2_gridNMin_2'
 results_file = '{}.pickle'.format(results_dir)
 home_dir = os.path.expanduser('~')
 results_dir_base = os.path.join(home_dir, 'results', 'el_ms', 'fig_4', 'flow_10D')
@@ -20,6 +22,23 @@ results_path = os.path.join(results_dir_base, results_dir, results_file)
 # Load data
 results_dict = f4.load_results(results_path)
 
-# %% Run analysis
+# %% Get list of directories
 
-f4.plot_flow_summary_hist(results_dict)
+# Get iterator for items in directory
+dirs = os.scandir(results_dir_base)
+for d in dirs:
+    # Check to see if the item is a directory
+    if not d.is_dir():
+        continue
+
+    # Define path to pickle data and check to see if it exists
+    f_name = '{}.pickle'.format(d.name)
+    file_path = os.path.join(results_dir_base, d.name, f_name)
+    P = pathlib.Path(file_path)
+    if not P.exists():
+        continue
+        
+    # Load results data and analyze
+    results_dict = f4.load_results(file_path)
+    f4.plot_flow_summary_hist(results_dict)
+    
