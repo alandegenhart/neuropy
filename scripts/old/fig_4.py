c1477e877779894547acb911473fe7b464a4bc44
Alan Degenhart <alandegenhart@gmail.com>
2020-06-22 14:36:46 -0700

    Cleaning up package contents -- removing old files, moving non-module code to the 'scripts' directory.

--- scripts/old/fig_4.py
+++ scripts/old/fig_4.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Sun Apr 26 14:21:03 2020
 
@@ -13,6 +11,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Custom modules
 sys.path.append('/Users/alandegenhart/src/')
@@ -78,7 +77,7 @@ gpfa_rot = neu.util.convertmat.convert_mat(rot_gpfa_path)['estParams']
 neu.el.proc.clean_gpfa_params(gpfa_int)
 neu.el.proc.clean_gpfa_params(gpfa_rot)
 
-# %% 
+# %% Check re-decoded trajectories
 
 # Check re-decode accuracy (intuitive)
 dif = neu.el.validation.redecode_cursor_pos(
@@ -90,7 +89,7 @@ dif = neu.el.validation.redecode_cursor_pos(
     df_rot['decodeSpikeCounts'], df_rot['decodeState'], dec_rot
 )
 
-# %%
+# %% Transform neural trajectories
 
 # Orthonormalize
 C_orth, T, s, VH = gpfa.orthogonalize(gpfa_int['C'])
@@ -105,39 +104,88 @@ U.iloc[0].shape
 U_orth = U.apply(lambda x: T @ x)
 U_orth.iloc[0].shape
 
-# Get start and end target positions
-start_pos = df_int['startPos']
-end_pos = df_int['targPos']
 
-# Define target combination
-start_pos = np.concatenate(start_pos)
-end_pos = np.concatenate(end_pos)
+# %% Get onset and offset of trajectories
+
+# Find onset/offset and add back to dataframe.
+onset_idx_int = df_int.apply(tmp.find_traj_onset, axis=1, result_type='expand')
+df_int['trajOnsetIdx'] = onset_idx_int['trajOnset']
+df_int['trajOffsetIdx'] = onset_idx_int['trajOffset']
+onset_idx_rot = df_rot.apply(tmp.find_traj_onset, axis=1, result_type='expand')
+df_rot['trajOnsetIdx'] = onset_idx_rot['trajOnset']
+df_rot['trajOffsetIdx'] = onset_idx_rot['trajOffset']
+
+# %% Get target conditions
+
+# Define target combinations
+start_pos = np.concatenate(df_int['startPos'])
+end_pos = np.concatenate(df_int['targPos'])
 targ_comb = np.concatenate([start_pos, end_pos], axis=1)
 uni_targ_comb = np.unique(targ_comb, axis=0)
 
+# Convert target combinations to trial conditions
 start_cond = tmp.get_targ_cond(df_int['startPos'])
 end_cond = tmp.get_targ_cond(df_int['targPos'])
 targ_cond = [''.join([s, e]) for s, e in zip(start_cond, end_cond)]
-mask = temp.get_targ_pairs(start_cond, end_cond)
-col_map = temp.define_color_map()
+mask = tmp.get_targ_pairs(start_cond, end_cond)
+col_map = tmp.define_color_map()
 
-tmp.plot_traj(df_int['decodeState'][mask], pd.Series(targ_cond)[mask], col_map,
+# %% Plot trajectories
+
+# Get new truncated trajectory values
+traj_valid = df_int.apply(
+    lambda x: x['decodeState'][:, x['trajOnsetIdx']:(x['trajOffsetIdx'] + 1)],
+    axis='columns')
+
+# Plot all trajectories
+tmp.plot_traj(
+    traj_valid[mask],
+    pd.Series(targ_cond)[mask],
+    col_map,
     col_mode='light',
     line_width=0.5,
     marker_size=7)
 
+# Plot average trajectories
+
+# Get unique paired target conditions
+uni_targ_cond = set([c for c, m in zip(targ_cond, mask) if m])
+
+for uc in uni_targ_cond:
+    # Get subset of trials for a single target condition and average
+    cond_mask = [True if tc == uc else False for tc in targ_cond]
+    avg_traj = tmp.spatial_average(traj_valid[cond_mask])
+
+    # Plot average trajectory
+    tmp.plot_single_traj(avg_traj, uc, col_map, line_width=2)
+
+# Display plot
+plt.show()
+
+
 # %% Next steps
 
-# - Find trajectory onset and offset indices
-# - Add target plotting
-# - Outer wrapper to generate plots (create figure layout, etc.)
-# - Spatial average method
-# - Implement FlowField class
+# Things to do soon:
+# TODO: flip y-axis of decode state (currently this is in the Host coordinate system)
+# TODO: trajectory plotting - int and rotated in the same projection
+# TODO: new color mapping with slightly different colors for intuitive and rotated
+# TODO: Implement FlowField class
 #    - Fit method
 #    - Can have child classes that implement different methods
 #    - Voxel method
 #    - Distance-weighted average method
 #    - NN-based method
-# - `load_test.py` will need to be turned into a more standalone function to
+
+# Analysis:
+# - Plot trajectories for different mappings (int and rotated) for each condition
+# - Calculate the flow field for a single condition/mapping dataset
+# - Method to plot a flow field
+# - Quantification of similarity for two flow fields
+# - Random projections
+
+# Things to do eventually:
+# TODO: Outer wrapper to generate plots (create figure layout, etc.)
+# TODO: Add target plotting
+# TODO: `load_test.py` will need to be turned into a more standalone function to
 #   batch iterate over datasets
-# - Look into Pandas pickle warnings
+# TODO: Look into Pandas pickle warnings in `load_test.py`
