8ede9e94d243b2830edf0ddde9baae5bb99a6d06
alandegenhart <alandegenhart@gmail.com>
2020-10-23 17:35:53 -0700

    Adding setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='Neuropy',
+	version='0.1',
+	description='Neural data analysis toolbox',
+	author='Alan Degenhart',
+	author_email='alandegenhart@gmail.com',
+	packages=['neuropy']
+)
