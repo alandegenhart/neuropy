b15909cbc0e3ec5758ed4394347a405c104469cb
alandegenhart <alandegenhart@gmail.com>
2021-03-10 08:23:37 -0800

    Removing EL paper 1 manuscript files (these have been removed to the 'el-paper1' repo.

--- scripts/ms/copy_results.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Define data paths
-SOURCE_DIR="/afs/ece.cmu.edu/project/nspg/adegenha/results/el_ms/fig_4/flow_10D"
-DEST_DIR="/Users/alandegenhart/results/el_ms/fig_4/"
-
-# Secure copy
-scp -r -P 22 adegenha@axon.ece.cmu.edu:$SOURCE_DIR $DEST_DIR
