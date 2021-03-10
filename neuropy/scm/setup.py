0ab67649e6b48a26bfb770f6427e00f6cd7991db
alandegenhart <alandegenhart@gmail.com>
2021-03-10 08:42:56 -0800

    Adding SCM module -- eventually the unused files here should be removed.

---
+++ neuropy/scm/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='Scientific Color Maps',
+	version='6.0',
+	description='Collection of scientific color maps.',
+	packages=['ScientificColourMaps']
+)
