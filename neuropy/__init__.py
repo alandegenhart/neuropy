ae695d9f87a38d01ef956c61921e15242caa6e23
alandegenhart <alandegenhart@gmail.com>
2021-03-11 07:15:53 -0800

    Removing import of el module (this has been removed from the repo).

--- neuropy/__init__.py
+++ neuropy/__init__.py
@@ -7,9 +7,7 @@ A neural analysis package implemented in Python.
 # Import sub-packages
 import neuropy.util
 import neuropy.analysis
-import neuropy.el
 
 # Import top-level modules
 import neuropy.array
 import neuropy.temp
-
